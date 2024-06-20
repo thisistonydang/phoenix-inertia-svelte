63062db69d002801c4681cbaa47fdbb8a61ffd44
Tony Dang <tony@tonydang.blog>
2024-06-20 10:24:26 -0700

    add comment for clarity

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,7 +28,7 @@ import axios from "axios";
 axios.defaults.xsrfHeaderName = "x-csrf-token";
 
 createInertiaApp({
-  page: undefined,
+  page: undefined, // This undefined prop is to avoid TS errors
   resolve: async (name) => await import(`./pages/${name}.svelte`),
   setup({ el, App }) {
     new App({ target: el, hydrate: true });
