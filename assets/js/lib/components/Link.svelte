305c4fa511dfc7f93430453f34fcfee4edd7f377
Tony Dang <tony@tonydang.blog>
2024-06-20 14:05:44 -0700

    add Link component

---
+++ assets/js/lib/components/Link.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let href: string;
+</script>
+
+<a
+  {href}
+  target="_blank"
+  rel="noopener noreferrer"
+  class="underline underline-offset-2 transition hover:no-underline"
+>
+  <slot />
+</a>
