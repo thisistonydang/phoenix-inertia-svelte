b1346cb00c34ed0672f275fdceb8d8f159c2900a
Tony Dang <tony@tonydang.blog>
2024-06-20 09:58:58 -0700

    add inertia plug

--- lib/app_web/router.ex
+++ lib/app_web/router.ex
@@ -8,6 +8,7 @@ defmodule AppWeb.Router do
     plug :put_root_layout, html: {AppWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Inertia.Plug
   end
 
   pipeline :api do
