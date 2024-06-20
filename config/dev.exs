54044b4b95d54a20551703506f9dc4b7c94f8972
Tony Dang <tony@tonydang.blog>
2024-06-20 13:49:21 -0700

    expose server for local network dev

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :app, App.Repo,
 config :app, AppWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
