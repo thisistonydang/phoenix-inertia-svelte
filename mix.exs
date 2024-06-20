cbf48759eaf8c2c81a07e10040e555d2913843d8
Tony Dang <tony@tonydang.blog>
2024-06-20 13:50:23 -0700

    add credo

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule App.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:inertia, "~> 0.7.0"}
+      {:inertia, "~> 0.7.0"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
