f0d5e096b7636ba9529c095f09fbc790078f3f6b
Tony Dang <tony@tonydang.blog>
2024-06-20 09:58:35 -0700

    add inertia helpers

--- lib/app_web.ex
+++ lib/app_web.ex
@@ -44,6 +44,7 @@ defmodule AppWeb do
 
       import Plug.Conn
       import AppWeb.Gettext
+      import Inertia.Controller
 
       unquote(verified_routes())
     end
@@ -73,6 +74,8 @@ defmodule AppWeb do
       # Import convenience functions from controllers
       import Phoenix.Controller,
         only: [get_csrf_token: 0, view_module: 1, view_template: 1]
+
+      import Inertia.HTML
 
       # Include general helpers for rendering HTML
       unquote(html_helpers())
